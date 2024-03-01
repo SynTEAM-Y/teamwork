a08229d2597aeb9f6960a12fe81a9f0627df51fb
Edvin Alestig <edvin.alestig@gmail.com>
2024-03-01 15:54:13 +0100

    Paige-Tarjan stability

--- src/main/java/com/syntm/analysis/Paige.java
+++ src/main/java/com/syntm/analysis/Paige.java
@@ -20,18 +20,25 @@ public class Paige implements java.io.Serializable {
      * transition system.
      * 
      * @param parameterStates All states in the parameter.
-     * @param indexedFamily A concurrent hashmap with an initial partition mapped to
-     *                      each state.
-     * @param transSystem   The transition system to be reduced.
-     * @param channels      A set of the available channels.
+     * @param indexedFamily   A concurrent hashmap with an initial partition mapped
+     *                        to each state.
+     * @param transSystem     The transition system to be reduced.
+     * @param channels        A set of the available channels.
      * @return An object callable with <code>run()</code> to reduce the transition
      *         system using the Paige-Tarjan algorithm.
      */
-    public Paige(Map<State, Set<Set<State>>> epsilonMap, 
+    public Paige(Map<State, Set<Set<State>>> epsilonMap,
             TS transSystem, Set<String> channels) {
         this.epsilonMap = new HashMap<>(epsilonMap);
         this.transSystem = transSystem;
         this.channels = channels; // Space complexity O(c)
+
+        // Add all incoming transitions to each state.
+        // Should be moved to wherever states are defined but
+        // it's impossible to find where that is.
+        for (Trans tr : transSystem.getTransitions()) { // O(m)
+            tr.destination.getPreTrans().add(tr);
+        }
 
     }
 
@@ -45,7 +52,7 @@ public class Paige implements java.io.Serializable {
         // O(c^2 m n^4)?
         while (!fixedPoint) { // O(c^2mn^4)
             HashMap<State, Set<Set<State>>> rhoResults = new HashMap<>();
-            
+
             for (State epsilon : epsilonMap.keySet()) { // O(c^2mn^3)
                 Set<Set<State>> rhoTemp = new HashSet<>(epsilonMap.get(epsilon));
                 for (String ch : channels) { // O(c^2mn^2)
@@ -67,7 +74,7 @@ public class Paige implements java.io.Serializable {
                     }
                 }
                 rhoResults.put(epsilon, rhoTemp);
-    
+
             }
 
             // update epsilonmap with the new partitions
@@ -85,10 +92,11 @@ public class Paige implements java.io.Serializable {
         for (State s : epsilonMap.keySet()) {
             // Gets the partition from the initial state.
             if (this.transSystem.initStateEqLabel(s)) {
-                if (epsilonMap.get(s).size() == 1) return this.transSystem;
+                if (epsilonMap.get(s).size() == 1)
+                    return this.transSystem;
                 rhoFinal.addAll(epsilonMap.get(s));
                 break;
-            } 
+            }
         }
 
         CompressedTS c = new CompressedTS("s-" + this.transSystem.getName());
@@ -97,22 +105,26 @@ public class Paige implements java.io.Serializable {
 
     /**
      * Def 5.4 in the paper
-     * <p> Complexity O(mn)
+     * <p>
+     * Complexity O(mn)
+     * 
      * @param p         The starting block
      * @param trEpsilon A transition (from p to ePrime)
      * @param ePrime    The block where the transition ends up.
      * @param channel   A channel name
      * @param epsilon   A state (our addition)
-     * @return          A possible splitter for the block.
+     * @return A possible splitter for the block.
      */
     private Set<State> applyEBisim(Set<State> p, Trans trEpsilon, // O(c+mn)=O(mn) or O(cn+mn+n^2)=O(mn)
-                                   Set<State> ePrime, String channel, 
-                                   State epsilon) {
+            Set<State> ePrime, String channel,
+            State epsilon) {
         Set<State> out = new HashSet<>();
-    
+
         if (epsilon.enable(epsilon, channel) // activating a channel, adds O(c)
                 && trEpsilon.getAction().equals(channel)) {
-            for (State s : p) { // *O(mn) or O(cn + n*max(c, m+n)) = O(max(cn, cn+mn+n^2))=O(cn+mn+n^2)=O(mn+n^2)=O(mn) but combines with the partition for loop outside the function
+            for (State s : p) { // *O(mn) or O(cn + n*max(c, m+n)) = O(max(cn,
+                                // cn+mn+n^2))=O(cn+mn+n^2)=O(mn+n^2)=O(mn) but combines with the partition for
+                                // loop outside the function
                 for (State sPrime : p) { // O(c + max(c, m+n)), c+m+n <= 3m -> O(m)
 
                     if (s.canExactSilent(s.getOwner(), s, channel) // O(c)
@@ -145,7 +157,8 @@ public class Paige implements java.io.Serializable {
                         }
                     }
 
-                    if (s.canDirectReaction(s.getOwner(), s, channel) // O(m+n) -- it is O(max(c, m+n)) = O(m+n) because c<=m
+                    if (s.canDirectReaction(s.getOwner(), s, channel) // O(m+n) -- it is O(max(c, m+n)) = O(m+n) because
+                                                                      // c<=m
                             && !sPrime.canExactSilent(sPrime.getOwner(), sPrime, channel)) {
                         if (sPrime.canDirectReaction(sPrime.getOwner(), sPrime, channel)) { // O(c)
                             if (ePrime.contains(s.takeDirectReaction(s.getOwner(), s, channel).getDestination())
@@ -155,10 +168,12 @@ public class Paige implements java.io.Serializable {
                                 out.add(s);
                                 out.add(sPrime);
                             }
-                        } else {  // O(m+n)
+                        } else { // O(m+n)
                             Set<State> reach = sPrime.weakBFS(sPrime.getOwner(), sPrime, channel); // O(m+n)
                             if (reach.size() != 1) { // O(m)
-                                for (State sReach : reach) { // O(m) It should be O(cn) but it's bounded by the number of transitions. It never checks a transition twice so it becomes O(m).
+                                for (State sReach : reach) { // O(m) It should be O(cn) but it's bounded by the number
+                                                             // of transitions. It never checks a transition twice so it
+                                                             // becomes O(m).
                                     if (sReach.canDirectReaction(sReach.getOwner(), sReach, channel)) { // O(c)
                                         if (ePrime
                                                 .contains(sReach.takeDirectReaction(sReach.getOwner(), sReach,
@@ -177,9 +192,9 @@ public class Paige implements java.io.Serializable {
                 }
             }
         }
-    
+
         if (out.isEmpty() && !epsilon.canTakeInitiative(epsilon.getOwner(), epsilon, channel) // O(cn)
-            && epsilon.getListen().getChannels().contains(channel)) {
+                && epsilon.getListen().getChannels().contains(channel)) {
             for (State s : p) { // O(cn)
                 if (s.canTakeInitiative(s.getOwner(), s, channel)) {
                     if (ePrime.contains(s.takeInitiative(s.getOwner(), s, channel).getDestination())) {
@@ -198,8 +213,10 @@ public class Paige implements java.io.Serializable {
                         }
                     }
                 }
-                if (!out.isEmpty() && !out.equals(p)) break;
-                if (out.equals(p))                    out.clear();
+                if (!out.isEmpty() && !out.equals(p))
+                    break;
+                if (out.equals(p))
+                    out.clear();
             }
         }
 
@@ -208,10 +225,12 @@ public class Paige implements java.io.Serializable {
 
     /**
      * A splitter function
-     * <p> Time complexity O(n)
-     * @param p         The block to split
-     * @param splitter  The block to split with
-     * @return          The resulting blocks
+     * <p>
+     * Time complexity O(n)
+     * 
+     * @param p        The block to split
+     * @param splitter The block to split with
+     * @return The resulting blocks
      */
     private Set<Set<State>> split(Set<State> p, Set<State> splitter) {
         Set<Set<State>> splitP = new HashSet<>();
@@ -224,26 +243,86 @@ public class Paige implements java.io.Serializable {
         return splitP;
     }
 
-    private Set<State> pre(String channel, Set<State> block){
+    /**
+     * pre(a, S) = {s | s a→ s′ for some s′ ∈ S}
+     * <p>
+     * Complexity O(nm) (with only one block's worth of n)
+     * 
+     * @param channel The channel to find transitions for
+     * @param block   The block with incoming transitions
+     * @return A set of all states with transitions to the block
+     */
+    private Set<State> pre(String channel, Set<State> block) {
         Set<State> finalSet = new HashSet<>();
-        for(State s: block){
-            for(Trans trans : s.getPreTrans()){
-                if(trans.action.equals(channel)){
+        for (State s : block) {
+            for (Trans trans : s.getPreTrans()) {
+                if (trans.action.equals(channel)) {
                     finalSet.add(trans.source);
                 }
-            } 
+            }
         }
         return finalSet;
     }
 
-    private boolean isStable(String channel ,Set<State> sBlock, Set<State> bBlock){
-        Set<State> s = pre(channel,sBlock);
+    /**
+     * A set B ⊆ Pr is stable with respect to a set S ⊆ Pr if
+     * either B ⊆ pre(S) or B ∩ pre(S) = ∅.
+     * 
+     * @param channel The channel to check stability on
+     * @param sBlock  Set S
+     * @param bBlock  Set B
+     * @return <code>true</code> if the set B is stable
+     */
+    private boolean isStable(String channel, Set<State> sBlock, Set<State> bBlock) {
+        Set<State> s = pre(channel, sBlock);
         Set<State> intersection = new HashSet<>(s);
         intersection.retainAll(bBlock);
-        if ( intersection.size() == 0 ||  s.containsAll(bBlock)){ 
-            return true;
-        }
-        return false;
+        return (intersection.isEmpty() || s.containsAll(bBlock));
     }
-    
+
+    /**
+     * A partition π is stable with respect to a set S if so is each block B ∈ π.
+     * 
+     * @param channel
+     * @param partition
+     * @param sBlock
+     * @return
+     */
+    private boolean partitionIsStable(String channel, Set<Set<State>> partition, Set<State> sBlock) {
+        for (Set<State> b : partition) {
+            if (!isStable(channel, sBlock, b))
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * A partition π is stable with respect to a partition π′ if π is stable with
+     * respect to each block in π′.
+     * 
+     * @param channel
+     * @param partition1
+     * @param partition2
+     * @return
+     */
+    private boolean partitionIsStableToPartition(String channel, Set<Set<State>> partition1,
+            Set<Set<State>> partition2) {
+        for (Set<State> b : partition2) {
+            if (!partitionIsStable(channel, partition1, b))
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * A partition π is stable if it is stable with respect to itself.
+     * 
+     * @param channel
+     * @param partition
+     * @return
+     */
+    private boolean isStableToItself(String channel, Set<Set<State>> partition) {
+        return partitionIsStableToPartition(channel, partition, partition);
+    }
+
 }
